1d8373096ce5baad66a0192d59fd484090f433b7
wzysgsg6 <ziwang324@gmail.com>
2026-09-16 22:59:34 +0800

    Fix stale card description, precache maskable icon, add favicon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-hub-v2';
+const CACHE_NAME = 'my-hub-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
   './icon.svg',
   './icon-192.png',
   './icon-512.png',
+  './icon-maskable-512.png',
   './apple-touch-icon.png'
 ];
 
